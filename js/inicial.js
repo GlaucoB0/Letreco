82e80fed2b44f8e869eebad768377254e5b566bd
GlaucoB0 <138785502+GlaucoB0@users.noreply.github.com>
2024-02-28 20:21:25 -0300

    novidades e ranking feito

--- js/inicial.js
+++ js/inicial.js
@@ -19,17 +19,34 @@ const loadNovidades = async () => {
   const limitdata = data.hotspots.slice(0, 3)
   return limitdata
 }
+//https://api.vagalume.com.br/rank.php?apikey=${}&type=mus&period=day&periodVal=${data}&scope=all&limit=6
+const loadRank = async () => {
+  const res = await fetch(`${baseUrl}/rank.php?apikey=${key}&type=mus&period=day&periodVal=${formatDate(tempo,"yyyymmdd")}&scope=all&limit=10
+  `);
+  const data = await res.json()
+  return data
+}
 
-
-
+const loadRankArt = async () => {
+  const res = await fetch(`${baseUrl}/rank.php?apikey=${key}&type=art&period=day&periodVal=${formatDate(tempo,"yyyymmdd")}&scope=all&limit=3
+  `);
+  const data = await res.json()
+  return data
+}
+const tempo = new Date()
+console.log(formatDate(tempo,"yyyymmdd"))
 
 const loadAllWithPromiseAll = async () => {
   //criar uma array para todas as funções acima, ele vai esperar todas os paramentros 
   const [Novidades, MaisTocadas, topArtistas] = await Promise.all([
     loadNovidades(),
+    loadRank(),
+    loadRankArt()
 
   ])
   showHotSpots(Novidades)
+  showRankMus(MaisTocadas)
+  showRankArt(topArtistas)
 }
 loadAllWithPromiseAll()
 
@@ -47,28 +64,92 @@ function acharUser(index){
   })
 }
 
+
+function formatDate(date, format) {
+    const map = {
+        mm: date.getMonth() + 1,
+        dd: date.getDate(),
+        yyyy: date.getFullYear(),
+    }
+    if(map.dd < 10){
+      map.dd = `0${map.dd}`
+    }
+    if(map.mm < 10){
+      map.mm = `0${map.mm}`
+    }
+    return format.replace(/mm|dd|yyyy/gi, matched => map[matched])
+}
+
+//Novidades
 function showHotSpots(spots){
-  
-  
-  console.log(spots)
   spots.map((spot)=>{
     const spotsSecc = document.createElement("article")
-    spotsSecc.classList.add(".hotSpots")
+    spotsSecc.classList.add("hotSpots")
     const novidades = document.querySelector(".novidades")
     novidades.appendChild(spotsSecc)
     spotsSecc.innerHTML = 
   `
-  <div class=${spot.id}>
+  <div class="hotMusic" id=${spot.id}>
     <div class="imagem">
       <img src="${spot.art_pic_src}" alt="foto do artista" class="imgArt">
-      <span class="nomeArt">${spot.title}</span>
-    </div>
-            
+      <span class="nomeArt">${(spot.title).toUpperCase()}</span>
+      <span class="Desc-mus">${spot.descr}</span>
+    </div> 
   </div>
   `
   console.log(spot)
-  })
-  
+  })  
 }
-/** main */
 
+
+//ranking de musicas
+function showRankMus(date){
+  const ranks = date.mus.day.all
+  ranks.map((rank,index)=>{
+    const rankSecc = document.createElement('section')
+    rankSecc.classList.add("rankMus")
+    const rankPai = document.querySelector(".maisAcessadas")
+    rankPai.appendChild(rankSecc)
+    rankSecc.innerHTML = 
+    `
+    <div class="music">
+      <span class="numRank">${index+1}</span>
+      <div class="info-musica">
+        <span class="nomeMusRank">${rank.name}</span>
+        <span class="nomeArtRank">${rank.art.name}</span>            
+      </div>
+    </div>
+    `
+  })
+}
+
+
+//Rank de artistas
+function showRankArt(art){
+  
+  const rankArtista = art.art.day.all
+  console.log(rankArtista)
+
+  
+
+  rankArtista.map((artista,index)=>{
+    const artSec = document.createElement('section')
+    artSec.classList.add("rankArt")
+    const rankArtpai = document.querySelector(".rankArtistas")
+    rankArtpai.appendChild(artSec)
+
+    artSec.innerHTML =`
+    <div class="art" id="${artista.id}">
+      <div class="imagemArt">
+        <img src="${artista.pic_medium}"></img> 
+        <span class="numRankArt">${index+1}°</span>     
+      </div>
+      <div class="info-artista">
+        <span class="nomeArtRank">${artista.name}</span>
+        <span class="views">${artista.views} views</span>
+      </div>
+
+    </div>
+    `
+  })
+}
